30a1661c84415affc96a9bd579d8d5f3ee079407
GabrielAraujo18 <marcelo.ms197@gmail.com>
2019-08-31 17:11:42 -0300

    Projeto Criado

---
+++ src/model/entities/Product.java
@@ -0,0 +1,83 @@
+package model.entities;
+
+public class Product {
+
+	private Integer id;
+	private String name;
+	private Double price;
+	private Integer Quantidade;
+
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(Integer id, String name, Double price, Integer quantidade) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		Quantidade = quantidade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantidade() {
+		return Quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		Quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", Quantidade=" + Quantidade + "]";
+	}
+
+}
