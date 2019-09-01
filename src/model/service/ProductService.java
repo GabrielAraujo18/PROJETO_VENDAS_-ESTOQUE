90dce0b6987b63222950e9344a861a0540839ca0
GabrielAraujo18 <marcelo.ms197@gmail.com>
2019-09-01 18:50:04 -0300

    Acesso ao Banco de Dados

--- src/model/service/ProductService.java
+++ src/model/service/ProductService.java
@@ -1,19 +1,17 @@
 package model.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.ProductDao;
 import model.entities.Product;
 
 public class ProductService {
 	
+	private ProductDao dao = DaoFactory.createProductDao();
+	
 	public List<Product> findAll(){
-		List<Product> list = new ArrayList<>();
-		list.add(new Product(1, "Camisa", 50.0, 10));
-		list.add(new Product(2, "Bermuda", 20.0, 15));
-		list.add(new Product(3, "Calça", 90.0, 20));
-		list.add(new Product(4, "Vestido", 80.0, 8));
-		return list;
+		return dao.findAll();
 	}
 	
 }
